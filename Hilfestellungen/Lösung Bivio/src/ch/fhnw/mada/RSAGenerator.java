9b759c74c6480efd05bbcff23653c6357871495d
LS <38795964+laurinscheuber@users.noreply.github.com>
2023-03-20 22:52:52 +0100

    added solution group basel

---
+++ Hilfestellungen/Lösung Bivio/src/ch/fhnw/mada/RSAGenerator.java
@@ -0,0 +1,25 @@
+package src.ch.fhnw.mada;
+
+public class RSAGenerator {
+  private static final String PATH = "generated/";
+
+  public static void main(String[] args) {
+    // Aufgabe 1
+    RSA.generateKeys(PATH + "pk.txt", PATH + "sk.txt");
+
+
+    // Aufgabe 2
+    String publicKey = FileHelper.readFile(PATH + "pk.txt");
+    String text = FileHelper.readFile("text.txt");
+
+    String encrypted = RSA.encrypt(text, publicKey);
+    FileHelper.writeFile(PATH + "chiffre.txt", encrypted);
+
+    // Aufgabe 3
+    String textD = RSA.decryptFile(PATH + "chiffre.txt", PATH + "sk.txt");
+    FileHelper.writeFile(PATH + "text-d.txt", textD);
+
+    // Vogts Nachricht
+    System.out.println("Entschlüsselter Text von Herrn Vogt: " + RSA.decryptFile("chiffre.txt", "sk.txt"));
+  }
+}
